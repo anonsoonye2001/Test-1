5880dac6ff724ee1fc6a510eae227240041f6388
anonsoonye2001 <54555566+anonsoonye2001@users.noreply.github.com>
2019-09-19 02:07:52 -0500

    Update Test-1.R

--- Test-1.R
+++ Test-1.R
@@ -1,12 +1,9 @@
-load(file="test1_data.Rdata")
-str(d)
-
 a<-c(5:14)
 a
-a[c(1,7)] #indexing 1 and 7 value
-a[c(1,7)]<-assign(a(c[]))
+a[c(1,7)] #indexing 1st and 7th value
+a[c(1,7)]<-c(20,26) # assigning 1st and 7th values to a new objects
+a
 
-edit
 
 a<-2
 b<-4
@@ -16,30 +13,34 @@ a>=b
 
 a<-c(5:14)
 a
-a[c(2,6,9)]
-
+a[c(2,6,9)] # indexing 2nd, 6th and 9th values.
+a[c(2,6,9)]<-c(21,25,28) # assigning 2nd,6th and 9th values to a new object
+a
 x=4
 y=3
 z=2
 ((z+x)*(z+y))/2
 10*(x-y)
 
-!=   # negative an object in r
-  
+!=   # negates an object in r
+a!=b  # means a not equal to b  
+
 a<- 32
 b<- 3
 a%%b  # %% is a modulus. It returns the remainder of the division of the vector
-32^ 10  # ^ is an exponent.multiples 32 by its value into 10 times.
+32^ 10  # ^ is an exponent.multiples 32 by its value into 10 times.32 raised to the power of 10.
 32%/%10 # is an integer division.32 is divided by 10. R returns the absolute value of the division
-'%nonso%'<-function(x,y){3*x+6*y}
+'%nonso%'<-function(x,y){3*x+6*y}  # special operator
 2%nonso%3
-u<-9 # left to right operator
+9->u # left to right operator
+u
+x=8;y=10;z=12  #3 objects with numbers assigned placed on the same line
+x;y;z  # code to print value
 
-x=8;y=10;z=12
-x;y;z
 4^1024 # special value to large for r to calculate. positive number gives Inf
 -4^1024 #special value to large for r to calculate. negative number gives -Inf
 Inf/Inf  # gives NaN. Not a number
+
 w<-c(1,2,3,4,5)
 length(w)
 length(w)<- 7
@@ -60,12 +61,29 @@ switcheroo.if.then(x="b")
 switcheroo.if.then(x="c")
 switcheroo.if.then(x)
 
-q<-("two",c(1:4),8,c(6,7,8,9))
+l<-list(x="two",y=c(1:4),z=8,w=c(a,b,d,e))
 
 y<-array(1:24,dim=c(4,3,2))
+y
 z<-matrix(11:20,nrow=5,ncol=2)
-# a matrix has one box (dimension) of data while array has n-dimension of data.
-
+# a matrix is a two dimensional vector while array is an extension of a vector to more than two dimension (n-dimension).
+z
 z[4,1]
 y[2,2,2]
+
+load(file="test1_data.Rdata")
+str(d)
+nrow(d)
+ncol(d)
+class(d$transect) # to reveal the data type in 'transect"
+str(d)  # to print the structure of the data frame
+#The five different data types are: chr,int,factor,POSIXct and num
+typeof(d$transect.id)
+
+d$tow<-as.factor(d$tow)  # to set as factor
+str(d)
+d$haul<-as.integer(d$haul)  # to set as integer
+str(d)
+d$sw.density<-NULL   # to remove "sw.density"column from the data.
+str(d)
 
